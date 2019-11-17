137a590adf4af48020d27977518639941cd8a5df
aliahmadcse <aliahmadcse@gmail.com>
2019-11-17 22:34:13 +0500

    Adds home page

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from application import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
-    return '<h2>Hello World</h2>'
+    return render_template("index.html")
