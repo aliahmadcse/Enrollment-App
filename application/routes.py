32951be8e6261cf88f1fd81f4eaf91b40a58c483
aliahmadcse <aliahmadcse@gmail.com>
2019-11-19 20:21:11 +0500

    Adds template for login,register and courses

--- application/routes.py
+++ application/routes.py
@@ -6,19 +6,19 @@ from flask import render_template
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html")
 
 
 @app.route('/login')
 def login():
-    return render_template("login.html", login=True)
+    return render_template("login.html")
 
 
 @app.route('/courses')
 def courses():
-    return render_template("courses.html", login=True)
+    return render_template("courses.html")
 
 
 @app.route('/register')
 def register():
-    return render_template("register.html", login=True)
+    return render_template("register.html")
