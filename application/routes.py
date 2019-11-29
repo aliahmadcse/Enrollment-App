e46d7ca07d053dd69fa9c368db62748dbd3e160a
aliahmadcse <aliahmadcse@gmail.com>
2019-11-29 14:42:09 +0500

    Changes routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,48 +1,5 @@
 from application import app
-<<<<<<< HEAD
-from flask import render_template, request, json, Response
-
-
-courseData = [
-    {
-        "courseID": "1111",
-        "title": "PHP 101",
-        "description": "Intro to PHP",
-        "credits": 3,
-        "term": "Fall, Spring"
-    },
-    {
-        "courseID": "2222",
-        "title": "Java 1",
-        "description": "Intro to Java Programming",
-        "credits": 4,
-        "term": "Spring"
-    },
-    {
-        "courseID": "3333",
-        "title": "Adv PHP 201",
-        "description": "Advanced PHP Programming",
-        "credits": 3,
-        "term": "Fall"
-    },
-    {
-        "courseID": "4444",
-        "title": "Angular 1",
-        "description": "Intro to Angular",
-        "credits": 3,
-        "term": "Fall, Spring"
-    },
-    {
-        "courseID": "5555",
-        "title": "Java 2",
-        "description": "Advanced Java Programming",
-        "credits": 4,
-        "term": "Fall"
-    }
-]
-=======
 from flask import render_template, request
->>>>>>> parent of d7b3f70... Adds routing pattern for api request
 
 
 @app.route('/')
