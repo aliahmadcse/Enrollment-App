ccd6f14fbbf904aeae2445edf61adadec97aa82b
aliahmadcse <aliahmadcse@gmail.com>
2019-12-02 00:46:22 +0500

    Adds flask-restplus extension and instantiated it

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
+from flask_restplus import Api
+
 
 app = Flask(__name__)
 # app configuration loading
 app.config.from_object(Config)
+
+# initializing api class
+api = Api()
+# providing app to the db object
+api.init_app(app)
 
 # instantiating the MongoEngine() class
 db = MongoEngine()
